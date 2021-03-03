d73857ba2b8ea4d43415605de0d40118487a367a
Chatmalow <maxime.pie.mail@gmail.com>
2021-03-03 14:26:04 +0100

    refactor: Move routes names into a specific routes file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 import Login from './components/pages/Login';
 import {BrowserRouter, NavLink, Route, Switch} from "react-router-dom";
+import {routes} from "./services/routes";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <div>
-          <NavLink to="/">Accueil</NavLink>
-          <NavLink to="/login">Login</NavLink>
-          <NavLink to="/signin">Sign in</NavLink>
+          <NavLink to={routes.HOME}>Accueil</NavLink>
+          <NavLink to={routes.LOGIN}>Login</NavLink>
+          <NavLink to={routes.SIGNIN}>Sign in</NavLink>
         </div>
         <Switch>
-          <Route path="/" exact>
+          <Route path={routes.HOME} exact>
             Bienvenue à l'accueil
           </Route>
-          <Route path="/login">
+          <Route path={routes.LOGIN}>
             <Login/>
           </Route>
-          <Route path="/signin">
+          <Route path={routes.SIGNIN}>
             S'inscrire arrive bientôt. Patience !
           </Route>
         </Switch>
---
+++ src/services/routes.js
@@ -0,0 +1,10 @@
+/**
+ * This file exports all the routes in the App.
+ * It is used to prevent from accidentally edit the routes.
+ */
+
+export const routes = {
+  HOME: '/',
+  LOGIN: '/login',
+  SIGNIN: '/signin',
+};
