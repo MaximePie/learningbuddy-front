f6424565e2df267e4a21f84ed6bb2a0cd7b3e3cf
Chatmalow <maxime.pie.mail@gmail.com>
2021-03-03 14:22:41 +0100

    feat: Add basic Routing structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Login from './components/pages/Login';
+import {BrowserRouter, NavLink, Route, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <div>
+          <NavLink to="/">Accueil</NavLink>
+          <NavLink to="/login">Login</NavLink>
+          <NavLink to="/signin">Sign in</NavLink>
+        </div>
+        <Switch>
+          <Route path="/" exact>
+            Bienvenue à l'accueil
+          </Route>
+          <Route path="/login">
+            <Login/>
+          </Route>
+          <Route path="/signin">
+            S'inscrire arrive bientôt. Patience !
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
