e6486c9b1cefd875f492edbf60769e65e6573a5e
Chatmalow <maxime.pie.mail@gmail.com>
2021-03-31 10:20:18 +0200

    feat: Add Font-awesome library

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import "@fortawesome/fontawesome-free/js/all.min";
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {routes} from "./services/routes";
 import Navbar from "./components/molecules/Navbar";
 import BackgroundIllustration from "./components/atoms/BackgroundIllustration";
+import ReactModule from "./components/pages/ReactModule";
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
         <Navbar/>
         <Switch>
           <Route path={routes.HOME} exact>
-            <h1>Bienvenue</h1>
+            <ReactModule/>
           </Route>
         </Switch>
       </div>
