2caa98f304d4c818617199caffe1cd3dc14fe3ee
Chatmalow <maxime.pie.mail@gmail.com>
2021-03-31 11:15:48 +0200

    feat: Install React Flow Renderer library

---
+++ src/components/molecules/LinkedNode.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Handle } from 'react-flow-renderer';
+
+/**
+ *
+ * This component is only used for React Flow Chart library.
+ * Do not use outside a ReactFlow component
+ *
+ * @param data
+ * @returns {*}
+ * @constructor
+ */
+export default function LinkedNode({ data }) {
+  const { url, icon, text } = data;
+  return (
+    <div className="LinkedNode">
+      <Handle
+        type="target"
+        position="left"
+        style={{ background: '#555' }}
+        onConnect={(params) => console.log('handle onConnect', params)}
+      />
+      <div className="LinkedNode__body">
+        <i className={`fas fab fa-${icon} LinkedNode__icon`}/>
+        <span className="LinkedNode__text">
+          {text}
+        </span>
+      </div>
+      <Handle
+        type="source"
+        position="right"
+        id="a"
+        style={{ top: 10, background: '#555' }}
+      />
+    </div>
+  );
+}
--- src/components/pages/ReactModule.jsx
+++ src/components/pages/ReactModule.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 import reactBanner from "../../images/reactBanner.jpeg";
+import ReactFlow from "react-flow-renderer";
+
+import LinkedNode from "../molecules/LinkedNode";
 
 export default function ReactModule() {
+  const customTypes = {
+    linkedNode: LinkedNode,
+  };
+  const elements = [
+    {
+      id: '1',
+      type: 'input', // input node
+      data: { label: 'Input Node' },
+      position: { x: 250, y: 25 },
+    },
+    // default node
+    {
+      id: '2',
+      // you can also pass a React component as a label
+      type: 'linkedNode',
+      data: { url: "haha.com", icon: "youtube", text: "Corrigez-vous grâce aux vidéos Youtube" },
+      position: { x: 100, y: 125 },
+    },
+    {
+      id: '3',
+      type: 'output', // output node
+      data: { label: 'Output Node' },
+      position: { x: 250, y: 250 },
+    },
+    { id: 'e1-2', source: '1', target: '2', animated: true},
+    { id: 'e2-3', source: '2', target: '3', animated: true },
+  ];
+
   return (
     <div className="ReactModule">
       <img className="ReactModule__banner" src={reactBanner} alt="React"/>
@@ -12,6 +43,13 @@ export default function ReactModule() {
       <div className="ReactModule__body">
         <div className="ReactModule__section">
           <h2>(?) Comment pratiquer ?</h2>
+          <ReactFlow
+            elements={elements}
+            nodeTypes={customTypes}
+            connectionLineStyle={{
+              stroke: '#ff0',
+            }}
+          />
         </div>
 
       </div>
