6ecc8c560c4c660a2120ef20caced8b8eea9060c
Chatmalow <maxime.pie.mail@gmail.com>
2021-03-31 11:28:06 +0200

    feat: Add style on ReactModule page

--- src/components/pages/ReactModule.jsx
+++ src/components/pages/ReactModule.jsx
@@ -35,23 +35,22 @@ export default function ReactModule() {
 
   return (
     <div className="ReactModule">
-      <img className="ReactModule__banner" src={reactBanner} alt="React"/>
-      <div className="ReactModule__header">
-        <i className="fas fa-heart ReactModule__header-icon"/>
-        <h1>Apprenez React par la pratique</h1>
+      <div className="ReactModule__banner-container">
+        <img className="ReactModule__banner" src={reactBanner} alt="React"/>
       </div>
-      <div className="ReactModule__body">
-        <div className="ReactModule__section">
-          <h2>(?) Comment pratiquer ?</h2>
-          <ReactFlow
-            elements={elements}
-            nodeTypes={customTypes}
-            connectionLineStyle={{
-              stroke: '#ff0',
-            }}
-          />
+      <div className="ReactModule__content">
+        <div className="ReactModule__header">
+          <h1><i className="fab fa-react ReactModule__header-icon"/>Apprenez React par la pratique</h1>
         </div>
-
+        <div className="ReactModule__body">
+          <div className="ReactModule__section">
+            <h2><i className="fas fa-question-circle"/> Comment pratiquer ?</h2>
+            <ReactFlow
+              elements={elements}
+              nodeTypes={customTypes}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
